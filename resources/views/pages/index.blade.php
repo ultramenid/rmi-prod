0dc072c5a08c6801e4da972de439a8b6942e8a3d
ultramenid <malichamdan@gmail.com>
2023-08-18 15:22:24 +0700

    tahun

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -268,7 +268,7 @@
         <div class="max-w-6xl mx-auto px-6 md:px-8 sm:py-16 py-8">
            <h1 class="uppercase text-center text-gray-300 sm:text-3xl text-xl font-bold mb-6">{{__('Companies')}}</h1>
            <p class="text-gray-300 sm:text-center text-left font-semibold leading-8">
-            {{__('The RMI Reports 2020 covers 38 companies from 19 home countries, including publicly-listed, stated-owned and private companies. These companies operate in more than 780 mine sites in 49 producing countries and control over 200 closed or suspended mine sites. The assesment covers most mined commodities, excluding oil and gas. it is possible to filter by company, using the list of all companies on the left hand side, and also to filter by producing country or commodity, using the fields below.')}}</p>
+            {{__('The RMI Reports')}} {{date('Y')}} {{__('covers 38 companies from 19 home countries, including publicly-listed, stated-owned and private companies. These companies operate in more than 780 mine sites in 49 producing countries and control over 200 closed or suspended mine sites. The assesment covers most mined commodities, excluding oil and gas. it is possible to filter by company, using the list of all companies on the left hand side, and also to filter by producing country or commodity, using the fields below.')}}</p>
         </div>
 
     </main>
@@ -344,7 +344,7 @@
           labels:economicdevelopment.name,
           xaxis: {
               type: 'category',
-              categories: []
+              categories: [],
           },
           yaxis: {
             type: 'string',
